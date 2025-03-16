dacf83e09a1c559a3cd16fdefe17370b00f8459a
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2025-03-16 19:41:12 +0530

    first commit

---
+++ src/StakingContract.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  ^0.8.13;
+
+contract StakingContract{
+
+    uint public totalStaked;
+    mapping(address=>uint256) public stakeBalances;
+
+
+
+    function stake() public payable{
+        require(msg.value >0,"The amount should be greater than 0");
+
+        stakeBalances[msg.sender]+=msg.value;
+        totalStaked+=msg.value;
+        
+    }
+
+    function unstake(uint256 amount){
+         require(msg.value >0,"The amount should be greater than 0");
+        require(amount =< stakeBalances[msg.sender],"Not enough balance");
+        payable(msg.sender).transfer(amount/2);
+        stakeBalances[msg.sender]-=amount/2;
+        totalStaked-=amount/2;
+    }
+}
