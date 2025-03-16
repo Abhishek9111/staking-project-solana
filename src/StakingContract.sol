a5c5da28585ffe92df232a4d6ab08d6c4ed72504
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2025-03-16 19:48:03 +0530

    fix: add more bugs, code improvements

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -9,19 +9,18 @@ contract StakingContract{
 
 
 
-    function stake() public payable{
-        require(msg.value >0,"The amount should be greater than 0");
+     function stake(uint amount) public payable{
+        require(amount >0,"The amount should be greater than 0");
 
-        stakeBalances[msg.sender]+=msg.value;
-        totalStaked+=msg.value;
+        stakeBalances[msg.sender]+=amount;
+        totalStaked+=amount;
         
     }
 
-    function unstake(uint256 amount) public payable{
-         require(msg.value >0,"The amount should be greater than 0");
-        require(amount =< stakeBalances[msg.sender],"Not enough balance");
+    function unstake(uint amount) public payable {
+        require(stakeBalances[msg.sender] >= amount,"Not enough balance");
         payable(msg.sender).transfer(amount/2);
-        stakeBalances[msg.sender]-=amount/2;
-        totalStaked-=amount/2;
+        stakeBalances[msg.sender]-=amount;
+        totalStaked-=amount;
     }
 }
